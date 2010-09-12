f7c0ae0c785474c5ac13c2106d9b07bc05606360
Martin Schürrer <martin@schuerrer.org>
2010-09-12 23:46:09 +0200

    Add migration to generator

--- lib/generators/devise/twitter_generator.rb
+++ lib/generators/devise/twitter_generator.rb
@@ -1,7 +1,12 @@
+require 'rails/generators/active_record'
+
 module Devise
   module Generators
-    class TwitterGenerator < Rails::Generators::NamedBase
+    class TwitterGenerator < ActiveRecord::Generators::Base
       source_root File.expand_path('../templates', __FILE__)
+      def add_migration
+        migration_template "migration.rb", "db/migrate/add_devise_twitter_fields_to_#{table_name}"
+      end
     end
   end
 end
