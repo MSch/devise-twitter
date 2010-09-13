4f9558c867eb7bb936e3d4d28ef47cbbdd8916b4
Martin Schürrer <martin@schuerrer.org>
2010-09-13 13:29:18 +0200

    Add rspec

--- devise-twitter.gemspec
+++ devise-twitter.gemspec
@@ -18,6 +18,7 @@ Gem::Specification.new do |s|
   s.add_dependency "devise", ">= 1.1.0"
   s.add_dependency "warden_oauth", "~> 0.1.1"
   s.add_development_dependency "bundler", ">= 1.0.0"
+  s.add_development_dependency "rspec", "~> 2.0.0.beta"
 
   s.files        = `git ls-files`.split("\n")
   s.executables  = `git ls-files`.split("\n").map{|f| f =~ /^bin\/(.*)/ ? $1 : nil}.compact
---
+++ spec/spec_helper.rb
@@ -0,0 +1,17 @@
+require 'rspec'
+
+# Requires supporting files with custom matchers and macros, etc,
+# in ./support/ and its subdirectories.
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
+
+RSpec.configure do |config|
+  # == Mock Framework
+  #
+  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
+  #
+  # config.mock_with :mocha
+  # config.mock_with :flexmock
+  # config.mock_with :rr
+  config.mock_with :rspec
+end
+
